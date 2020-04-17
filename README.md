c173ec4a665ac0e8ca30a4e65aff2abe41cdcec6
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-04-17 20:47:30 +0900

    Add GitHub Actions, Update documents

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Parallel Media Downloader
 
+[![Test](https://github.com/yukihiko-shinoda/parallel-media-downloader/workflows/Test/badge.svg)](https://github.com/yukihiko-shinoda/parallel-media-downloader/actions?query=workflow%3ATest)
+[![codecov](https://codecov.io/gh/yukihiko-shinoda/parallel-media-downloader/branch/master/graph/badge.svg)](https://codecov.io/gh/yukihiko-shinoda/parallel-media-downloader)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/parallelmediadownloader)](https://pypi.org/project/parallelmediadownloader/)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/parallelmediadownloader)](https://pypi.org/project/parallelmediadownloader/)
+[![Twitter URL](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Fgithub.com%2Fyukihiko-shinoda%2Fparallel-media-downloader)](http://twitter.com/share?text=Parallel%20Media%20Downloader&url=https://pypi.org/project/parallelmediadownloader/&hashtags=python)
+
 This project helps you to download media file.
--- setup.py
+++ setup.py
@@ -7,26 +7,41 @@ from setuptools import find_packages
 
 def main():
     """This function implements build settings."""
-    with open('README.md', 'r', encoding='utf8') as file:
+    with open("README.md", "r", encoding="utf8") as file:
         readme = file.read()
 
     setup(
-        name='parallelmediadownloader',
-        version='0.0.1',
-        description='This project helps you to download media file.',
+        name="parallelmediadownloader",
+        version="0.0.1",
+        description="This project helps you to download media file.",
         long_description=readme,
-        long_description_content_type='text/markdown',
-        author='Yukihiko Shinoda',
-        author_email='yuk.hik.future@gmail.com',
+        long_description_content_type="text/markdown",
+        author="Yukihiko Shinoda",
+        author_email="yuk.hik.future@gmail.com",
         packages=find_packages(exclude=("tests*",)),
         package_data={"parallelmediadownloader": ["py.typed"]},
-        install_requires=[
-            'aiohttp',
-        ],
+        install_requires=["aiohttp"],
+        dependency_links=[],
         url="https://github.com/yukihiko-shinoda/parallel-media-downloader",
         keywords="parallel media download downloader image jpg jpeg png gif aiohttp",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Information Technology",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Multimedia :: Graphics",
+            "Topic :: System :: Archiving",
+            "Topic :: System :: Archiving :: Backup",
+            "Typing :: Typed",
+        ],
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
